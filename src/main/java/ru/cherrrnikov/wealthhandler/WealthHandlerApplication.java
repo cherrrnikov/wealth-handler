bd2118ffa080682f0342b060dfd1193fcc0bcee4
chera <chernikoviam@gmail.com>
2026-08-02 21:35:45 +0300

    feat: add infrastructure setup - docker-compose, flyway migration, app config

---
+++ src/main/java/ru/cherrrnikov/wealthhandler/WealthHandlerApplication.java
@@ -0,0 +1,11 @@
+package ru.cherrrnikov.wealthhandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WealthHandlerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WealthHandlerApplication.class, args);
+    }
+}
