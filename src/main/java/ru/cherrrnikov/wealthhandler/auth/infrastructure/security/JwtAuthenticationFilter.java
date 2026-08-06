bf579c7f18cf7440009c88d78d45b8b6bf11ff75
chera <chernikoviam@gmail.com>
2026-08-06 22:48:08 +0300

    feat: add JwtAuthenticationFilter and UserDetailsServiceImpl

---
+++ src/main/java/ru/cherrrnikov/wealthhandler/auth/infrastructure/security/JwtAuthenticationFilter.java
@@ -0,0 +1,51 @@
+package ru.cherrrnikov.wealthhandler.auth.infrastructure.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = authorizationHeader.substring("Bearer ".length());
+
+        if (jwtService.validateToken(token)) {
+            String email = jwtService.extractEmail(token);
+
+            UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                    userDetails, null, userDetails.getAuthorities()
+            );
+
+            authentication.setDetails(
+                    new WebAuthenticationDetailsSource().buildDetails(request)
+            );
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
---
+++ src/main/java/ru/cherrrnikov/wealthhandler/auth/infrastructure/security/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package ru.cherrrnikov.wealthhandler.auth.infrastructure.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.cherrrnikov.wealthhandler.auth.application.port.out.UserRepository;
+import ru.cherrrnikov.wealthhandler.auth.domain.Role;
+import ru.cherrrnikov.wealthhandler.auth.domain.User;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPasswordHash())
+                .authorities(user.getRoles().stream()
+                        .map(Role::getName)
+                        .toArray(String[]::new)
+                )
+                .build();
+    }
+}
