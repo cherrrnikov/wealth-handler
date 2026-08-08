b75aeb55291b298870ec06846ccc82ec1aa18473
chera <chernikoviam@gmail.com>
2026-08-08 15:57:27 +0300

    refactor: add BCrypt cost factor 12 and configure NimbusJWTEncoder/Decoder beans

---
+++ src/main/java/ru/cherrrnikov/wealthhandler/auth/infrastructure/security/JwtConfig.java
@@ -0,0 +1,36 @@
+package ru.cherrrnikov.wealthhandler.auth.infrastructure.security;
+
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class JwtConfig {
+    private final JwtProperties jwtProperties;
+
+    @Bean
+    public JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey.Builder(jwtProperties.getPublicKey())
+                .privateKey(jwtProperties.getPrivateKey())
+                .build();
+        JWKSource<SecurityContext> jwkSource = new ImmutableJWKSet<>(new JWKSet(jwk));
+
+        return new NimbusJwtEncoder(jwkSource);
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(jwtProperties.getPublicKey()).build();
+    }
+}
